4522df8aa7976fd4cad248d87abde537e1e5c784
xximxxhatedxx <ghostr210305@gmail.com>
2023-12-24 10:50:26 +0100

    start

---
+++ src/main/java/com/librarysystem/librarysystem/Config.java
@@ -0,0 +1,9 @@
+package com.librarysystem.librarysystem;
+
+public class Config {
+    protected String dbHost = "localhost";
+    protected String dbPort = "3306";
+    protected String dbUser = "root";
+    protected String dbPass = "12345678";
+    protected String dbName= "LibrarySystem";
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,10 @@
+module com.librarysystem.librarysystem {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires java.sql;
+    requires mysql.connector.j;
+
+
+    opens com.librarysystem.librarysystem to javafx.fxml;
+    exports com.librarysystem.librarysystem;
+}
