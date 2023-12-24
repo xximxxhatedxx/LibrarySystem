801c82b0b55cc6ac9654e313f374fa930e53be46
xximxxhatedxx <ghostr210305@gmail.com>
2023-12-24 14:16:43 +0100

    start

---
+++ src/main/java/com/librarysystem/librarysystem/Controller.java
@@ -0,0 +1,4 @@
+package com.librarysystem.librarysystem;
+
+public class Controller {
+}
--- src/main/java/com/librarysystem/librarysystem/DatabaseHandler.java
+++ src/main/java/com/librarysystem/librarysystem/DatabaseHandler.java
@@ -2,6 +2,7 @@ package com.librarysystem.librarysystem;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 
 public class DatabaseHandler extends Config {
@@ -17,5 +18,17 @@ public class DatabaseHandler extends Config {
         dbConnection = DriverManager.getConnection(connectionString, dbUser, dbPass);
 
         return dbConnection;
+    }
+
+    public void createBook(String author, String name, String genre, int number)
+            throws SQLException, ClassNotFoundException {
+        String query = "INSERT INTO books (author, name, genre, number) VALUES (?,?,?,?)";
+        PreparedStatement preparedStatement = getDbConnection().prepareStatement(query);
+        preparedStatement.setString(1, author);
+        preparedStatement.setString(2, name);
+        preparedStatement.setString(3, genre);
+        preparedStatement.setInt(4, number);
+
+        preparedStatement.executeUpdate();
     }
 }
