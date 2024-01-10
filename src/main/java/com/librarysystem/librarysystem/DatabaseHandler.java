9362cfdfc30f7673fe8f7222d52898284fd8a917
Curiosity <bezradina514@gmail.com>
2024-01-10 16:42:11 +0200

    hello

--- src/main/java/com/librarysystem/librarysystem/DatabaseHandler.java
+++ src/main/java/com/librarysystem/librarysystem/DatabaseHandler.java
@@ -4,7 +4,7 @@ import java.sql.*;
 import java.util.*;
 
 public class DatabaseHandler extends Config {
-    Connection dbConnection = null;
+//    Connection dbConnection = null;
 
     public Connection getDbConnection()
             throws SQLException{
