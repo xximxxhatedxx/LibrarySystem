3a9681f7a7549743de70c0ef47cf4617f46d132d
Curiosity <bezradina514@gmail.com>
2024-03-14 18:55:10 +0200

    new function for registration and log in

--- src/main/java/com/librarysystem/librarysystem/LogInController.java
+++ src/main/java/com/librarysystem/librarysystem/LogInController.java
@@ -1,6 +1,7 @@
 package com.librarysystem.librarysystem;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
 
@@ -18,12 +19,29 @@ public class LogInController extends Main{
     @FXML
     private Button goBack;
 
+    public void showErrorEmptyAlert() {
+        Alert alert = new Alert(Alert.AlertType.ERROR);
+        alert.setTitle("Error!");
+        alert.setHeaderText("Oops, I think you got it wrong.");
+        alert.setContentText("Fields must not be empty");
+
+        alert.showAndWait();
+    }
     @FXML
     void initialize() throws IOException {
         logInButton.setOnAction(event ->{
             String email =emailLogInField.getText();
             String password = passwordLogInField.getText();
 
+            if (email.isEmpty() || password.isEmpty()){
+                showErrorEmptyAlert();
+            }
+            DatabaseHandler db = new DatabaseHandler();
+            try{
+                db.getUserByEmail(email, password);
+            }catch (Exception e){
+                System.out.println(e);
+            }
         });
         goToRegistrationButton.setOnAction(event -> {
             try {
@@ -31,6 +49,7 @@ public class LogInController extends Main{
             } catch (IOException e) {
                 System.out.println(e);
             }
+
         });
         goBack.setOnAction(event -> {
             try {
